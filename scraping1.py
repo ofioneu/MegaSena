70572387ce6a91788dc826c9ce0fc2adba514071
Fabio Martins <fmfabio.0991@yahoo.com.br>
2019-11-06 23:07:29 -0300

    webscraping numeros em vetor c/ indice dia semana e separado por funçoes

--- scraping1.py
+++ scraping1.py
@@ -1,19 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import date
 
-page = requests.get('https://www.sorteonline.com.br/mega-sena/resultados/')
-print(page)
-soup = BeautifulSoup(page.text, "html.parser")
+def scraping():
+    page = requests.get('https://www.sorteonline.com.br/mega-sena/resultados/')
+    print(page)
+    soup = BeautifulSoup(page.text, "html.parser")
 
-mydivs = soup.find("div", {"class": "result result-default center"})
-filhos=mydivs.findChildren("li")
+    mydivs = soup.find("div", {"class": "result result-default center"})
+    filhos=mydivs.findChildren("li")
 
-vet_filhos =[]
+    vet_filhos =[]
 
-for child in filhos:
-    vet_filhos.append(child.text)
+    for child in filhos:
+        vet_filhos.append(child.text)
 
-print(vet_filhos)
+    print(vet_filhos)
 
+def indiceSemana():
+    hoje=date.today()
+    indice=hoje.weekday()
+    print(indice)
+    if(indice==2 or indice==5):
+        scraping()
 
-
+indiceSemana()
