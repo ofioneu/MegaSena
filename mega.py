df6ad0005a1960f9e7dd1bc8375e7ae81bcbacdd
Fabio Martins <fmfabio.0991@yahoo.com.br>
2019-11-14 17:49:06 -0300

    Tratando os dados com o excel

--- mega.py
+++ mega.py
@@ -25,30 +25,38 @@ finally:
        vet =[]
        soup = BeautifulSoup(open("d_mega.html"), "html.parser")
        mydivs = soup.find("table")
-       dados_html=mydivs.find_all('tr')
+       dados_html=mydivs.find_all('tr') #tr retorna um bloco com 21
        print(len(dados_html))
 
-       #for que passa os ddos html para um vetor
+       #for que passa os ddos html para um vetor a cada 21 td corresponde a um resultado
        for i in dados_html:
-          vet.append(i.text)   
+           if (i != None):
+               vet.append(i.text)   
                 
-       print(vet[2])
+       #print(vet[2])
        
-       insert_query = "insert into megasena (id, concurso, data_sorteio, primeira_dez, segunda_dez, terceira_dez, quarta_dez, quinta_dez, sexta_dez, arrecadacao_total, ganhadores_sena, cidade, uf, rateio_sena, ganhadores_quina, rateio_quina, ganhadores_quadra, rateio_quadra, acomulado, valor_acomulado, estimativa_premio, acomulado_mega_da_virada) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-       dados_insert = (vet[2])
+       #insert_query = "insert into megasena (id, concurso, data_sorteio, primeira_dez, segunda_dez, terceira_dez, quarta_dez, quinta_dez, sexta_dez, arrecadacao_total, ganhadores_sena, cidade, uf, rateio_sena, ganhadores_quina, rateio_quina, ganhadores_quadra, rateio_quadra, acomulado, valor_acomulado, estimativa_premio, acomulado_mega_da_virada) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+       #dados_insert = (vet[2])
 
-       cur.execute(insert_query, dados_insert)
+       #cur.execute(insert_query, dados_insert)
 
-       recset = cur.fetchall()
-       print(recset)
-
-      # tamanho_vet = len(vet)
+       #recset = cur.fetchall()
+       #print(recset)
+       tes=[]
+       tes1=[]
+       tamanho_vet = len(vet)
        #for que le os dados das posições de um vetor
        #for percorre in range(tamanho_vet):
-          # for elementos in vet[percorre]:
-             #  print(elementos)
+           #tes1.append(percorre)
+           #for elementos in vet[percorre]:
+               #for p in range(21):              
+                   #tes.append(elementos)
+               #del(vet[0:21])
             
-       
-
+       #print(tes) 
+       for u in vet[2]:
+           tes1.append(u)
+       print(vet[2]) 
+       print(tes1)
 
 scraping()
